4fe900ed2c2fdd0e64d9f7e0fa6c5b49aae7077d
devran-hub <73471656+devran-hub@users.noreply.github.com>
2024-12-14 11:59:17 +0300

    Son değişiklikler

--- main.c
+++ main.c
@@ -3,52 +3,94 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct StNode{
+struct StNode
+{
 	int data;
-	struct StNode* parent;
-	struct StNode* left;
-	struct StNode* right;
+	int freq;
+	struct StNode *parent;
+	struct StNode *left;
+	struct StNode *right;
 };
 typedef struct StNode StNode;
 typedef StNode *StNodePtr;
 
-StNodePtr Insert(StNodePtr , int, StNodePtr*);
-void preorder(StNodePtr);
-void Splay(StNodePtr* , StNodePtr* );
+StNodePtr Insert(StNodePtr, int, StNodePtr *, int *);
+void preorder(StNodePtr, FILE *);
+void Splay(StNodePtr *, StNodePtr *, int *);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	int TimeForModSplay = 0;
+	int TimeForSplay = 0;
+
 	StNodePtr root = NULL;
+	StNodePtr modRoot = NULL;
+
 	StNodePtr lastAdded = NULL;
+	StNodePtr modLastAdded = NULL;
 
-	root = Insert(root, 10, &lastAdded);
-	//Splay(&root, &lastAdded);
+	FILE *file = fopen("input.txt", "r");
+	FILE *output = fopen("output.txt", "w");
+	int key = 0;
+	char x = fgetc(file);
 
-	root = Insert(root, 11, &lastAdded);
-//	Splay(&root, &lastAdded);
+	while (!feof(file))
+	{
+		if (x - '0' < 10 && x - '0' >= 0)
+			key += key * 10 + (x - '0');
+		else
+		{
+			root = Insert(root, key, &lastAdded, &TimeForSplay);
+			modRoot = Insert(modRoot, key, &modLastAdded, &TimeForModSplay);
+			if (lastAdded->freq == 0)
+				Splay(&root, &lastAdded, &TimeForSplay);
 
-	root = Insert(root, 2, &lastAdded);
-//	Splay(&root, &lastAdded);
+			if (modRoot->freq < modLastAdded->freq)
+				Splay(&modRoot, &modLastAdded, &TimeForModSplay);
 
-	root = Insert(root, 1, &lastAdded);
-//	Splay(&root, &lastAdded);
+			key = 0;
+		}
+		x = fgetc(file);
+	}
+	fprintf(output, "\n Pre-order demonstration of splay tree: ");
+	preorder(root, output);
+	fprintf(output, "\n Comparison time of splay tree: %d \n", TimeForSplay);
+	fprintf(output, "\n Pre-order demonstration of mod-splay tree: ");
+	preorder(modRoot, output);
+	fprintf(output, "\n Comparison time of mod-splay tree: %d", TimeForModSplay);
 
-	root = Insert(root, 4, &lastAdded);
-//	Splay(&root, &lastAdded);
-	root = Insert(root, 3, &lastAdded);
-	root = Insert(root, 9, &lastAdded);
-	root = Insert(root, 5, &lastAdded);
-	root = Insert(root, 8, &lastAdded);
-	root = Insert(root, 7, &lastAdded);
-	root = Insert(root, 6, &lastAdded);
-	root = Insert(root, 6, &lastAdded);
-	Splay(&root, &lastAdded);
-	preorder(root);
-	return 0;
+	/*
+		root = Insert(root, 10, &lastAdded);
+		// Splay(&root, &lastAdded);
+
+		root = Insert(root, 11, &lastAdded);
+		//	Splay(&root, &lastAdded);
+
+		root = Insert(root, 2, &lastAdded);
+		//	Splay(&root, &lastAdded);
+
+		root = Insert(root, 1, &lastAdded);
+		//	Splay(&root, &lastAdded);
+
+		root = Insert(root, 4, &lastAdded);
+		//	Splay(&root, &lastAdded);
+		root = Insert(root, 3, &lastAdded);
+		root = Insert(root, 9, &lastAdded);
+		root = Insert(root, 5, &lastAdded);
+		root = Insert(root, 8, &lastAdded);
+		root = Insert(root, 7, &lastAdded);
+		root = Insert(root, 6, &lastAdded);
+		root = Insert(root, 6, &lastAdded);
+		Splay(&root, &lastAdded);
+		preorder(root);
+		return 0;*/
 }
-void zigZig(StNodePtr* lastAdded, StNodePtr* root, int leftOrRight){
+void zigZig(StNodePtr *lastAdded, StNodePtr *root, int leftOrRight)
+{
 	// 0 for left, 1 for right.
-	//left zigzig
-	if (leftOrRight == 0){
+	// left zigzig
+	if (leftOrRight == 0)
+	{
 
 		(*lastAdded)->parent->parent->left = (*lastAdded)->parent->right;
 
@@ -71,15 +113,17 @@ void zigZig(StNodePtr* lastAdded, StNodePtr* root, int leftOrRight){
 
 		if ((*lastAdded)->parent == NULL)
 			*root = *lastAdded;
-		else{
-			if ( (*lastAdded)->parent != NULL && (*lastAdded)->parent->right->data == (*lastAdded)->right->right->data)
+		else
+		{
+			if (((*lastAdded)->parent->right != NULL) && (*lastAdded)->parent->right->data == (*lastAdded)->right->right->data)
 				(*lastAdded)->parent->right = *lastAdded;
-			else if ( (*lastAdded)->parent != NULL && (*lastAdded)->parent->left->data == (*lastAdded)->right->right->data)
+			else if (((*lastAdded)->parent->left != NULL) && (*lastAdded)->parent->left->data == (*lastAdded)->right->right->data)
 				(*lastAdded)->parent->left = *lastAdded;
 		}
 	}
-	//right zizig
-	else{
+	// right zizig
+	else
+	{
 
 		(*lastAdded)->parent->parent->right = (*lastAdded)->parent->left;
 
@@ -103,18 +147,20 @@ void zigZig(StNodePtr* lastAdded, StNodePtr* root, int leftOrRight){
 		if ((*lastAdded)->parent == NULL)
 			*root = *lastAdded;
 
-		else{
-			if ( (*lastAdded)->parent->left->data == (*lastAdded)->left->left->data)
+		else
+		{
+			if (((*lastAdded)->parent->left != NULL) && (*lastAdded)->parent->left->data == (*lastAdded)->left->left->data)
 				(*lastAdded)->parent->left = *lastAdded;
-			else if ( (*lastAdded)->parent->right->data == (*lastAdded)->left->left->data)
+			else if (((*lastAdded)->parent->right != NULL) && (*lastAdded)->parent->right->data == (*lastAdded)->left->left->data)
 				(*lastAdded)->parent->right = *lastAdded;
 		}
 	}
 }
 
-void leftRotation(StNodePtr* lastAdded, StNodePtr* root){
+void leftRotation(StNodePtr *lastAdded, StNodePtr *root)
+{
 
-	//counter clockwise rotation.
+	// counter clockwise rotation.
 	(*lastAdded)->parent->right = (*lastAdded)->left;
 	(*lastAdded)->left = (*lastAdded)->parent;
 	(*lastAdded)->parent = (*lastAdded)->parent->parent;
@@ -124,18 +170,20 @@ void leftRotation(StNodePtr* lastAdded, StNodePtr* root){
 
 	(*lastAdded)->left->parent = (*lastAdded);
 
-	if ( (*lastAdded)->parent == NULL)
+	if ((*lastAdded)->parent == NULL)
 		*root = *lastAdded;
-	else{
-		if ( ((*lastAdded)->parent->left != NULL) && (*lastAdded)->parent->left->data == (*lastAdded)->left->data)
+	else
+	{
+		if (((*lastAdded)->parent->left != NULL) && (*lastAdded)->parent->left->data == (*lastAdded)->left->data)
 			(*lastAdded)->parent->left = (*lastAdded);
-		else if ( ((*lastAdded)->parent->right != NULL) && (*lastAdded)->parent->right->data == (*lastAdded)->left->data)
+		else if (((*lastAdded)->parent->right != NULL) && (*lastAdded)->parent->right->data == (*lastAdded)->left->data)
 			(*lastAdded)->parent->right = (*lastAdded);
 	}
 }
-void rightRotation(StNodePtr* lastAdded, StNodePtr* root){
+void rightRotation(StNodePtr *lastAdded, StNodePtr *root)
+{
 
-	//clockwise rotation.
+	// clockwise rotation.
 	(*lastAdded)->parent->left = (*lastAdded)->right;
 	(*lastAdded)->right = (*lastAdded)->parent;
 	(*lastAdded)->parent = (*lastAdded)->parent->parent;
@@ -145,69 +193,101 @@ void rightRotation(StNodePtr* lastAdded, StNodePtr* root){
 
 	(*lastAdded)->right->parent = *lastAdded;
 
-	if ( (*lastAdded)->parent == NULL)
+	if ((*lastAdded)->parent == NULL)
 		*root = *lastAdded;
-	else{
-		if (  (*lastAdded)->parent->left != NULL && (*lastAdded)->parent->left->data == (*lastAdded)->right->data)
+	else
+	{
+		if ((*lastAdded)->parent->left != NULL && (*lastAdded)->parent->left->data == (*lastAdded)->right->data)
 			(*lastAdded)->parent->left = (*lastAdded);
-		else if ( (*lastAdded)->parent->right != NULL && (*lastAdded)->parent->right->data == (*lastAdded)->right->data)
+		else if ((*lastAdded)->parent->right != NULL && (*lastAdded)->parent->right->data == (*lastAdded)->right->data)
 			(*lastAdded)->parent->right = (*lastAdded);
 	}
 }
-void Splay(StNodePtr* root, StNodePtr* lastAdded){
+void Splay(StNodePtr *root, StNodePtr *lastAdded, int *cTime)
+{
 
-	if ( (*lastAdded)->parent != NULL){
+	if ((*lastAdded)->parent != NULL)
+	{
 
-		if ( (*lastAdded)->parent->parent == NULL){
+		if ((*lastAdded)->parent->parent == NULL)
+		{
 
-			if ((*lastAdded)->parent->left != NULL && (*lastAdded)->parent->left->data == (*lastAdded)->data )
+			if ((*lastAdded)->parent->left != NULL && (*lastAdded)->parent->left->data == (*lastAdded)->data)
+			{
 				rightRotation(lastAdded, root);
-			else if ( (*lastAdded)->parent->right != NULL && (*lastAdded)->parent->right->data == (*lastAdded)->data)
+				(*cTime)++;
+			}
+			else if ((*lastAdded)->parent->right != NULL && (*lastAdded)->parent->right->data == (*lastAdded)->data)
+			{
 				leftRotation(lastAdded, root);
+				(*cTime)++;
+			}
 		}
-		else if ( (*lastAdded)->parent->parent->left != NULL && (*lastAdded)->parent->parent->left->left != NULL && (*lastAdded)->parent->parent->left->left->data == (*lastAdded)->data){
+		else if ((*lastAdded)->parent->parent->left != NULL && (*lastAdded)->parent->parent->left->left != NULL && (*lastAdded)->parent->parent->left->left->data == (*lastAdded)->data)
+		{
 			zigZig(lastAdded, root, 0);
-			Splay(root, lastAdded);
+			*cTime += 2;
+			Splay(root, lastAdded, cTime);
 		}
-		else if ( (*lastAdded)->parent->parent->right != NULL && (*lastAdded)->parent->parent->right->right != NULL && (*lastAdded)->parent->parent->right->right->data == (*lastAdded)->data){
+		else if ((*lastAdded)->parent->parent->right != NULL && (*lastAdded)->parent->parent->right->right != NULL && (*lastAdded)->parent->parent->right->right->data == (*lastAdded)->data)
+		{
 			zigZig(lastAdded, root, 1);
-			Splay(root, lastAdded);
+			*cTime += 2;
+			Splay(root, lastAdded, cTime);
 		}
-		else if ( (*lastAdded)->parent->parent->left != NULL && (*lastAdded)->parent->parent->left->right != NULL && (*lastAdded)->parent->parent->left->right->data == (*lastAdded)->data){
+		else if ((*lastAdded)->parent->parent->left != NULL && (*lastAdded)->parent->parent->left->right != NULL && (*lastAdded)->parent->parent->left->right->data == (*lastAdded)->data)
+		{
 			leftRotation(lastAdded, root);
 			rightRotation(lastAdded, root);
-			Splay(root, lastAdded);
+			*cTime += 2;
+			Splay(root, lastAdded, cTime);
 		}
-		else if ( (*lastAdded)->parent->parent->right != NULL && (*lastAdded)->parent->parent->right->left != NULL && (*lastAdded)->parent->parent->right->left->data == (*lastAdded)->data){
+		else if ((*lastAdded)->parent->parent->right != NULL && (*lastAdded)->parent->parent->right->left != NULL && (*lastAdded)->parent->parent->right->left->data == (*lastAdded)->data)
+		{
 			rightRotation(lastAdded, root);
 			leftRotation(lastAdded, root);
-			Splay(root, lastAdded);
+			*cTime += 2;
+			Splay(root, lastAdded, cTime);
 		}
 	}
 }
 
-StNodePtr Insert(StNodePtr root, int data, StNodePtr* lastAdded){
-	if (root == NULL){
+StNodePtr Insert(StNodePtr root, int data, StNodePtr *lastAdded, int *cTime)
+{
+	if (root == NULL)
+	{
 		StNodePtr newNode = malloc(sizeof(StNode));
 		newNode->data = data;
 		newNode->left = newNode->right = newNode->parent = NULL;
 		*lastAdded = newNode;
 		return newNode;
 	}
-	else if (data < root->data ){
-		root->left = Insert(root->left, data, lastAdded);
+	else if (data < root->data)
+	{
+		(*cTime)++;
+		root->left = Insert(root->left, data, lastAdded, cTime);
 		root->left->parent = root;
 	}
-	else if (data > root->data){
-		root->right = Insert(root->right, data, lastAdded);
+	else if (data > root->data)
+	{
+		(*cTime)++;
+		root->right = Insert(root->right, data, lastAdded, cTime);
 		root->right->parent = root;
+	}
+	else if (data == root->data)
+	{
+		(*cTime)++;
+		(root->freq)++;
+		*lastAdded = root;
 	}
 	return root;
 }
-void preorder(StNodePtr root){
-	if (root != NULL){
-		printf("%d ", root->data);
-		preorder(root->left);
-		preorder(root->right);
+void preorder(StNodePtr root, FILE *file)
+{
+	if (root != NULL)
+	{
+		fprintf(file, "%d ", root->data);
+		preorder(root->left, file);
+		preorder(root->right, file);
 	}
 }
